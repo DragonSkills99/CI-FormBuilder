e3999f99c15a31a7271ad473b50122ed006d04f1
DragonSkills99 <kurt.mueller123@gmx.de>
2018-02-21 16:30:08 +0100

    Create index.php

---
+++ Installation/index.php
@@ -0,0 +1,4 @@
+<p>To install this extension you have to put the <strong>FormBuilder.php</strong> into the following path:</p>
+<pre><code>application/libraries/
+</code></pre>
+<p>That's it. Now you've installed the extension and you're able to use it.</p>
